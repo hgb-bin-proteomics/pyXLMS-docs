d5f75d5daceeaa54d79532f37f993556ae82f3d1
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-05-14 15:46:25 +0200

    init nextra docs

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>pyXLMS</span>,
+  project: {
+    link: 'https://github.com/hgb-bin-proteomics/pyXLMS',
+  },
+  docsRepositoryBase: 'https://github.com/hgb-bin-proteomics/pyXLMS-docs',
+  footer: {
+    text: 'pyXLMS - Documentation',
+  },
+}
+
+export default config
