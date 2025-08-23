d45e845855f47943c8c57498948130d0c8539c7a
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-08-24 01:52:33 +0200

    add plot crosslink type distribution

--- content/docs/plotting/_meta.js
+++ content/docs/plotting/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'index': 'Available Figures and Plot Types',
+  'plot_crosslink_type_distribution': 'Plotting Crosslink Type Distribution',
   'plot_peptide_pair_distribution': 'Plotting Peptide Pair Distribution',
   'plot_protein_distribution': 'Plotting Protein Distribution',
   'plot_score_distribution': 'Plotting Score Distribution',
