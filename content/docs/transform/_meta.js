52215744e96e07b5c43f7ee1f5e0125494c5fdf2
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-08-24 02:08:54 +0200

    add intersection

--- content/docs/transform/_meta.js
+++ content/docs/transform/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'index': 'Available Data Transformations',
   'aggregate': 'Uniqueness and Aggregation',
-  'filter_target_decoy': 'Filtering Target and Decoy Matches'
+  'filter_target_decoy': 'Filtering Target and Decoy Matches',
+  'intersection': 'Intersections of Crosslinks and Crosslink-Spectrum-Matches'
 }
