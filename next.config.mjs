722eb2d6b6afe66fe1a4981b4f9563ed5b0342d9
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-09-02 22:42:53 +0200

    add export

--- next.config.mjs
+++ next.config.mjs
@@ -8,4 +8,8 @@ const withNextra = nextra({
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
+  basePath: process.env.PAGES_BASE_PATH,
+  images: { unoptimized: true },
+  output: 'export',
+  distDir: 'out',
 })
