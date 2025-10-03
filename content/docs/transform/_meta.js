30dc4cff39aeddd8729d82953f0ba73a6221dc17
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-04 01:19:24 +0200

    add annotation docs

--- content/docs/transform/_meta.js
+++ content/docs/transform/_meta.js
@@ -4,5 +4,6 @@ export default {
   'aggregate': 'Uniqueness and Aggregation',
   'validate': 'False-Discovery-Rate Estimation and Validation',
   'filter': 'Filtering Crosslink-Spectrum-Matches and Crosslinks',
+  'reannotate_positions': 'Annotation',
   'intersection': 'Intersections of Crosslinks and Crosslink-Spectrum-Matches'
 }
