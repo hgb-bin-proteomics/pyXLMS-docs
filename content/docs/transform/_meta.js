8e7b5357065161df78845b53485bf26619986ff7
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-04 01:03:40 +0200

    add summary docs

--- content/docs/transform/_meta.js
+++ content/docs/transform/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'index': 'Available Data Transformations',
+  'summary_and_util': 'Result Summary and Utility Functions',
   'aggregate': 'Uniqueness and Aggregation',
   'validate': 'False-Discovery-Rate Estimation and Validation',
   'filter': 'Filtering Crosslink-Spectrum-Matches and Crosslinks',
