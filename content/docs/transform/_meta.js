156a96a251b53659dd82a881f4b3b2b39898d60f
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-04 00:20:34 +0200

    add validation docs

--- content/docs/transform/_meta.js
+++ content/docs/transform/_meta.js
@@ -1,6 +1,7 @@
 export default {
   'index': 'Available Data Transformations',
   'aggregate': 'Uniqueness and Aggregation',
+  'validate': 'False-Discovery-Rate Estimation and Validation',
   'filter': 'Filtering Crosslink-Spectrum-Matches and Crosslinks',
   'intersection': 'Intersections of Crosslinks and Crosslink-Spectrum-Matches'
 }
