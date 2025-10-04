9a3506ef00c505d8950ba46a9c858634855907cf
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-04 23:17:54 +0200

    add mzid parser docs

--- content/docs/parsers/_meta.js
+++ content/docs/parsers/_meta.js
@@ -3,5 +3,6 @@ export default {
   'custom': 'Reading pyXLMS and Custom Files',
   'maxlynx': 'Reading MaxLynx/MaxQuant Result Files',
   'merox': 'Reading MeroX Result Files',
-  'msannika': 'Reading MS Annika Result Files'
+  'msannika': 'Reading MS Annika Result Files',
+  'mzid': 'Reading mzIdentML Result Files'
 }
