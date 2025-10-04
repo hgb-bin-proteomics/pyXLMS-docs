9195ae118b993897222e50eb8154bfea09477b46
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-04 23:12:10 +0200

    add merox docs

--- content/docs/parsers/_meta.js
+++ content/docs/parsers/_meta.js
@@ -2,5 +2,6 @@ export default {
   'index': 'Introduction',
   'custom': 'Reading pyXLMS and Custom Files',
   'maxlynx': 'Reading MaxLynx/MaxQuant Result Files',
+  'merox': 'Reading MeroX Result Files',
   'msannika': 'Reading MS Annika Result Files'
 }
