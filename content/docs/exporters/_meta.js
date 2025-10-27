0a389bb277589806346c53404d641f091b124e0f
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-27 13:14:03 +0100

    add to xlms tools docs

--- content/docs/exporters/_meta.js
+++ content/docs/exporters/_meta.js
@@ -7,5 +7,6 @@ export default {
   'to_pyxlinkviewer': 'Exporting Crosslinks to PyXlinkViewer',
   'to_xifdr': 'Exporting Crosslink-Spectrum-Matches to xiFDR',
   'to_xinet_xiview': 'Exporting Crosslinks to xiNET and xiVIEW',
-  'to_xlinkdb': 'Exporting Crosslinks to XLinkDB'
+  'to_xlinkdb': 'Exporting Crosslinks to XLinkDB',
+  'to_xlmstools': 'Exporting Crosslinks to xlms-tools'
 }
