ec0b5728272afa8265c7368297ad7c867a23954f
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-27 12:41:05 +0100

    add to impxfdr docs

--- content/docs/exporters/_meta.js
+++ content/docs/exporters/_meta.js
@@ -1,5 +1,6 @@
 export default {
   'index': 'Introduction',
   'to_alphalink2': 'Exporting Crosslinks to AlphaLink2',
+  'to_impxfdr': 'Exporting Results to IMP-X-FDR',
   'to_msannika': 'Exporting to MS Annika Format'
 }
