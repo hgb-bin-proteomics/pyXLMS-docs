a7335b1b55955206ee291425e7d4437c5d00aaaa
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-27 13:00:35 +0100

    add to xifdr docs

--- content/docs/exporters/_meta.js
+++ content/docs/exporters/_meta.js
@@ -4,5 +4,6 @@ export default {
   'to_impxfdr': 'Exporting Results to IMP-X-FDR',
   'to_msannika': 'Exporting Results to MS Annika Format',
   'to_proxl': 'Exporting Crosslink-Spectrum-Matches to ProXL',
-  'to_pyxlinkviewer': 'Exporting Crosslinks to PyXlinkViewer'
+  'to_pyxlinkviewer': 'Exporting Crosslinks to PyXlinkViewer',
+  'to_xifdr': 'Exporting Crosslink-Spectrum-Matches to xiFDR'
 }
