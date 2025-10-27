5116b35de595f0cac562ea15f5fa7bf92c886cc0
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2025-10-27 12:45:31 +0100

    add to ms annika docs

--- content/docs/exporters/_meta.js
+++ content/docs/exporters/_meta.js
@@ -2,5 +2,5 @@ export default {
   'index': 'Introduction',
   'to_alphalink2': 'Exporting Crosslinks to AlphaLink2',
   'to_impxfdr': 'Exporting Results to IMP-X-FDR',
-  'to_msannika': 'Exporting to MS Annika Format'
+  'to_msannika': 'Exporting Results to MS Annika Format'
 }
